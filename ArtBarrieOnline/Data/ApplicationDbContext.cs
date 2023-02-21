85da6aa9fa449a803c4ac1d04392f426f75eaa0f
singh <harmanpreets2003@gmail.com>
2023-02-21 18:59:06 -0500

    controllers

--- ArtBarrieOnline/Data/ApplicationDbContext.cs
+++ ArtBarrieOnline/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ArtBarrieOnline.Models;
 
 namespace ArtBarrieOnline.Data
 {
@@ -9,5 +10,8 @@ namespace ArtBarrieOnline.Data
 			: base(options)
 		{
 		}
+		public DbSet<ArtBarrieOnline.Models.Art> Art { get; set; }
+		public DbSet<ArtBarrieOnline.Models.Category> Category { get; set; }
+		public DbSet<ArtBarrieOnline.Models.Organization> Organization { get; set; }
 	}
 }
