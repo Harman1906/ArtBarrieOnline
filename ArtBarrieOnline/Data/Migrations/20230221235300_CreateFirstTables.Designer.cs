6a7c26c0e0560a8c144aec855c81260ca8185a20
singh <harmanpreets2003@gmail.com>
2023-02-21 18:55:35 -0500

    database

--- ArtBarrieOnline/Data/Migrations/20230221235300_CreateFirstTables.Designer.cs
+++ ArtBarrieOnline/Data/Migrations/20230221235300_CreateFirstTables.Designer.cs
@@ -4,6 +4,7 @@ using ArtBarrieOnline.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ArtBarrieOnline.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230221235300_CreateFirstTables")]
+    partial class CreateFirstTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ArtBarrieOnline/Data/Migrations/20230221235300_CreateFirstTables.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtBarrieOnline.Data.Migrations
+{
+    public partial class CreateFirstTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
