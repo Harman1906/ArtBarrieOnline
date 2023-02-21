a62ba0c9e191182e233814cad021cff486e1dd0d
singh <harmanpreets2003@gmail.com>
2023-02-21 18:49:10 -0500

    SQL

--- ArtBarrieOnline/Models/Art.cs
+++ ArtBarrieOnline/Models/Art.cs
@@ -1,6 +1,33 @@
-﻿namespace ArtBarrieOnline.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtBarrieOnline.Models
 {
     public class Art
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? Pic { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        public DateTime Date_Added { get; set; }
+
+        public string? CategoryId { get; set; }
+
+        public string? OrganizationId { get; set;}
+
+
+        public Category Category { get; set; }
+
+        public Organization Organization { get; set; }
     }
 }
